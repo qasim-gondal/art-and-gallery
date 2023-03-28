606c8aa7fdc78a84af82aafc128b6c9d87198470
qasim-gondal <qgondal48@gmail.com>
2023-03-28 23:30:00 +0500

    mailer

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,9 @@
 class ArtistsController < ApplicationController
     before_action :authenticate_user!
     def index
-        @artists=Artist.all
+    @artists=Artist.all
     end
+    
     def show 
           @artist=Artist.find(params[:id])
     end
