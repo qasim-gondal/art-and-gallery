14ce3096a86bc40223466f5a063e5ca2233746da
qasim-gondal <qgondal48@gmail.com>
2023-05-12 16:05:53 +0500

    gem rolify added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+
   validates :email, presence: true, length: { maximum: 255 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable  rolify
 end
